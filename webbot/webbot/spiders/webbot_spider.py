5ed1a155eb44c03af53f4c0915fdfd5cc1cc46b4
mobilesns <luohualiang@hotmail.com>
2014-11-11 21:46:14 +0900

    minor changes

--- webbot/webbot/spiders/webbot_spider.py
+++ webbot/webbot/spiders/webbot_spider.py
@@ -2,10 +2,10 @@
 
 from scrapy.spider import Spider 
 from scrapy.selector import Selector
-from amazon.items import AmazonItem
+from webbot.items import WebbotItem
 
 class AmazonSpider(Spider):
-    name = "amazon"
+    name = "webbot"
     allowed_domains = ["amazon.co.jp"]
     start_urls = [
         "http://www.amazon.co.jp/s/ref=sr_in_-2_p_89_36?rh=n%3A160384011%2Cn%3A%21161669011%2Cn%3A170303011%2Cn%3A170329011%2Cp_89%3A花王%28Kao%29&bbn=170329011&ie=UTF8&qid=1415441255&rnid=2321255051"
@@ -19,7 +19,7 @@ class AmazonSpider(Spider):
         mechans = sel.xpath('//a/span[@class="bld lrg red"]/text()')
         items = []
         for mechan in mechans:
-            item = AmazonItem()
+            item = WebbotItem()
             item['price'] = mechan.extract() 
             items.append(item)
         return items
